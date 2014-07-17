57119aa5e92fac17dbf6b3df54202334b490ab86
ahawkins <adam@hawkins.io>
2014-07-17 23:22:57 +0200

    Pass config options in via switches

--- lib/decks/cli.rb
+++ lib/decks/cli.rb
@@ -3,10 +3,18 @@ require 'thor'
 module Decks
   class CLI < Thor
     desc 'release PATH', 'Prepare release using PATH'
-    option :format, required: true, type: :string
+    option :format, required: true, type: :string, aliases: '-f'
+    option :artist, type: :string, aliases: '-a'
+    option :name, type: :string, aliases: '-n'
+    option :year, type: :numeric, aliases: '-y'
+    option :compilation, type: :boolean, aliases: '-c'
     def release(path)
       configuration = Release.from_path path do |config|
         config.format = options[:format]
+        config.artists = Array(options[:artist])
+        config.name = options[:name]
+        config.year = options[:year].to_i
+        config.compilation = options[:compilation]
       end
 
       UI.start configuration, $stdout, $stdin
